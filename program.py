902fca5b57de2d8ec6191e9b23775eecb42a27ed
NourhanAdelis1 <138725302+NourhanAdelis1@users.noreply.github.com>
2023-08-04 19:07:16 +0000

    jj

--- program.py
+++ program.py
@@ -1,1 +1,16 @@
+import random
 
+rnn = random.randint(10, 99)
+hehe = int(input("please guess a number between 10 and 99!"))
+for i in range(5):
+    if hehe > rnn :
+        hehe = int(input("Wrong! Less!\n"))
+    elif hehe < rnn :
+        hehe = int(input("Wrong! More!\n"))
+    elif hehe == rnn : 
+        hehe = int(input("Correct! You win!\n"))
+        break
+    else:
+        hehe = int(input("Please write a number between 10-99!"))
+
+print(f"{rnn} was the answer")
